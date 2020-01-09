04430354b0f5e244bce7063e0e7a577113aea97d
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-01-08 21:52:00 -0300

    Adjusting the text format due to issues with displaying

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
 let $ = document.querySelector.bind(document);
 let hoje = moment().locale('pt-br');
 let arrayMeses = [];
-let ulLista = $('.lista');
-let ulContador = $('.contador');
+let lista = $('.lista');
+let contador = $('.contador');
 let qtde = 0;
-
-function renderizar(index) {
-    let liLista = document.createElement('li');
-    let liContador = document.createElement('li');
-    liLista.appendChild(document.createTextNode(arrayMeses[index]));
-    ulLista.appendChild(liLista);
-    liContador.appendChild(document.createTextNode(qtde));
-    ulContador.appendChild(liContador);
-}
 
 for (let index = 0; index < 12; index++) {
     const hojeFormatado = hoje.format('MMMM').toLowerCase();
     arrayMeses.push(hojeFormatado);
     hoje.add(1, 'months');
-
-    renderizar(index);
+    
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(qtde));
+    contador.appendChild(li);
+    lista.innerHTML = arrayMeses;
 }
 
 $('.btn').addEventListener("click", () => {
     arrayMeses.filter((item, index) => {
-        if(item.indexOf($('input').value) != -1) {
-            renderizar(index);
-        }
+        
     });
 });
